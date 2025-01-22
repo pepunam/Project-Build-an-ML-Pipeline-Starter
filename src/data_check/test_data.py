abdd65747859cb215e2e56164a434ee11b732162
pepunam <pepunam4@gmail.com>
2025-01-22 03:45:12 -0600

    Commit changes

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data):
+    """
+    Test that the dataset contains a reasonable number of rows
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Test that all property prices are within the specified range
+    """
+    assert data['price'].between(min_price, max_price).all()
